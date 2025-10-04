196c077db55ce7ae55e0ad1a33175377d6103a04
GeraskoNikita <nikitagerasko@mail.ru>
2025-10-04 16:05:58 +0600

    Add files via upload

---
+++ src/main/java/com/example/lesson_31/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lesson_31
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lesson_31.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportFragmentManager.beginTransaction().replace(binding.carContainer.id, CarFragment()).commit()
+
+    }
+
+
+
+
+}
